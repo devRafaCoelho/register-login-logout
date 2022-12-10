521b86651e9336906adbe5d701b97e18410540a2
devRafaCoelho <108550900+devRafaCoelho@users.noreply.github.com>
2022-12-10 10:26:59 -0300

    created simple form register style

--- frontend/src/components/FormRegister/styles.js
+++ frontend/src/components/FormRegister/styles.js
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
-export const Form = styled.form``;
-export const InputContainer = styled.div``;
-export const Button = styled.button``;
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+`;
+export const Button = styled.button`
+    all: unset;
+    box-sizing: border-box;
+
+    min-height: 30px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 4px;
+    background-color: blueviolet;
+    color: white;
+`;
